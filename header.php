566c5f90df73bc5eb3be965fda45959ab62286fb
Jonathan Roy <jroy@optimumweb.ca>
2012-06-19 15:27:03 -0400

    Style

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php get_template_part('head'); ?>
+	<?php wpbp_wrap_before(); ?>
+	<div id="wrap" role="document">
+		<?php wpbp_header_before(); ?>
+		<header id="header" role="banner">
+			<?php wpbp_header_inside_before(); ?>
+            <div class="container <?php wpbp_option('container_class'); ?>">
+            	<div class="grid_3">
+					<section id="site-title">
+						<a id="logo" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></a>
+					</section>
+			    </div>
+			    <div class="grid_9">
+					<nav id="main-nav" role="navigation">
+						<div class="container">
+							<?php wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'walker' => 'Description_Walker' ) ); ?>
+							<div class="clear"></div>
+						</div>
+					</nav>
+			    </div>
+            </div>
+            <?php wpbp_header_inside_after(); ?>
+		</header>
+		<?php wpbp_header_after(); ?>
