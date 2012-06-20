9f7a3b8777a0753d08b1353581b7c70bc7dcdd9d
Jonathan Roy <jroy@optimumweb.ca>
2012-06-20 17:54:57 -0400

    Style

--- loop.php
+++ loop.php
@@ -11,30 +11,36 @@
 	<?php wpbp_post_before(); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php wpbp_post_inside_before(); ?>
-		<?php if ( has_post_thumbnail() ) : ?>
-			<section class="post-thumbnail">
-				<?php the_post_thumbnail('thumbnail'); ?>
-			</section>
-		<?php endif; ?>
-		<header class="post-header">
-			<section class="post-meta">
-				<time class="post-date updated" datetime="<?php the_time('c'); ?>" pubdate><?php printf(__('Posted on %s at %s', 'wpbp'), get_the_time(__('l, F jS, Y', 'wpbp')), get_the_time()); ?></time>
-			</section>
-			<h2 class="post-title">
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</h2>
-		</header>
-		<section class="post-content">
-			<?php if (is_archive() || is_search()) : // Only display excerpts for archives and search ?>
-				<?php the_excerpt(); ?>
-			<?php else : ?>
-				<?php the_content( __("Lire la suite", 'sosendettement') ); ?>
-			<?php endif; ?>
-		</section>
-		<footer class="post-footer">
-			<p class="post-tags"><?php the_tags(); ?></p>
-		</footer>
-		<div class="clear"></div>
+		<div class="container <?php wpbp_option('container_class'); ?>">
+			<div class="grid_3 alpha">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<section class="post-thumbnail">
+						<?php the_post_thumbnail('thumbnail'); ?>
+					</section>
+				<?php endif; ?>
+			</div>
+			<div class="grid_5 omega">
+				<header class="post-header">
+					<h2 class="post-title">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</h2>
+					<section class="post-meta">
+						<time class="post-date updated" datetime="<?php the_time('c'); ?>" pubdate><?php printf(__('Posted on %s at %s', 'wpbp'), get_the_time(__('l, F jS, Y', 'wpbp')), get_the_time()); ?></time>
+					</section>
+				</header>
+				<section class="post-content">
+					<?php if (is_archive() || is_search()) : // Only display excerpts for archives and search ?>
+						<?php the_excerpt(); ?>
+					<?php else : ?>
+						<?php the_content( __("Lire la suite", 'sosendettement') ); ?>
+					<?php endif; ?>
+				</section>
+				<footer class="post-footer">
+					<p class="post-tags"><?php the_tags(); ?></p>
+				</footer>
+				<div class="clear"></div>
+			</div>
+		</div>
 		<?php wpbp_post_inside_after(); ?>
 	</article>
 	<hr />
