efa31d2215d53dec05205d3ccc284e1fbb4b8e88
Jonathan Roy <jroy@optimumweb.ca>
2012-06-20 19:14:58 -0400

    Style

---
+++ footer.php
@@ -0,0 +1,24 @@
+		<?php get_template_part('map'); ?>
+		
+		<?php wpbp_footer_before(); ?>
+		<footer id="footer" role="contentinfo">
+			<?php wpbp_footer_inside_before(); ?>
+			<div class="container <?php wpbp_option('container_class'); ?>">
+				<nav id="footer-nav">
+					<?php wp_nav_menu( array( 'theme_location' => 'secondary_navigation' ) ); ?>
+					<div class="clear"></div>
+				</nav>
+				<div id="copy">
+					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+				</div>
+			</div>
+			<?php wpbp_footer_inside_after(); ?>
+		</footer>
+		<?php wpbp_footer_after(); ?>
+	</div>
+
+<?php wp_footer(); ?>
+<?php wpbp_footer(); ?>
+
+</body>
+</html>
---
+++ map.php
@@ -0,0 +1,5 @@
+<section id="map">
+	<div class="container">
+		<iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.ca/maps/ms?hl=fr&amp;msa=0&amp;msid=213368521201099456780.00048386a074d826d0b40&amp;ie=UTF8&amp;ll=45.572321,-73.707769&amp;spn=0.635284,0.83951&amp;t=m&amp;vpsrc=6&amp;output=embed"></iframe>
+	</div>
+</section>
