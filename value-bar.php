0d04ef69e89e97698868846ad4176b3de2a8a686
Jonathan Roy <jroy@optimumweb.ca>
2012-06-20 15:22:27 -0400

    Style

--- value-bar.php
+++ value-bar.php
@@ -6,23 +6,23 @@
 			</div>
 		</div>
 	</div>
-	<div id="value-bar-bullets">
+	<div id="value-bar-items">
 		<div class="container <?php wpbp_option('container_class'); ?>">
 			<div class="grid_4">
-				<div class="bullet">
-					<span>1</span>
+				<div class="item">
+					<div class="bullet">1</div>
 					<p><?php _e("<em>Obtenez de l’information véridique</em> et à jour sur les options qui s’offrent à vous pour vous sortir du gouffre de l’endettement.", 'sosendettement'); ?></p>
 				</div>
 			</div>
 			<div class="grid_4">
-				<div class="bullet">
-					<span>2</span>
+				<div class="item">
+					<div class="bullet">2</div>
 					<p><?php _e("<em>Trouvez un professionnel de confiance</em> près de chez vous qui fera l’évaluation sans frais de votre situation pour vous permettre d’y voir plus clair.", 'sosendettement'); ?></p>
 				</div>
 			</div>
 			<div class="grid_4">
-				<div class="bullet">
-					<span>3</span>
+				<div class="item">
+					<div class="bullet">3</div>
 					<p><?php _e("<em>Protégez-vous contre les arnaques</em> et soyez assuré que la solution choisie est réaliste. Sortez vous de l’endettement une fois pour toutes.", 'sosendettement'); ?></p>
 				</div>
 			</div>
