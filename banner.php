9ec3dcd439982fea6d2200754bf01d00bd197395
Jonathan Roy <jroy@optimumweb.ca>
2012-06-20 20:39:20 -0400

    Style

--- banner.php
+++ banner.php
@@ -16,7 +16,7 @@
 								</ul>
 								<div class="title2"><?php _e("Vous pouvez vous en sortir!", 'sosendettement'); ?></div>
 							<?php endif; ?>
-							<div><a class="button large arrow scroll-to" href="#map"><?php _e("Trouvez de l'aide", 'sosendettement'); ?></a></div>
+							<div><a class="button xlarge arrow scroll-to" href="#map"><?php _e("Trouvez de l'aide", 'sosendettement'); ?></a></div>
 						</div>
 					</section>
 				</div>
