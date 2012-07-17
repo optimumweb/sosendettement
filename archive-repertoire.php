80f1f8a853457e5f1c646449198d0277d3ce04ff
Jonathan Roy <jroy@optimumweb.ca>
2012-07-17 14:04:13 -0400

    Style

---
+++ archive-repertoire.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+	<?php wpbp_content_before(); ?>
+	<section id="content">
+		<div class="container <?php wpbp_option('container_class'); ?>">
+			<div class="<?php wpbp_option('main_class'); ?>">
+				<?php wpbp_main_before(); ?>
+				<section id="main" role="main">
+					<?php wpbp_main_inside_before(); ?>
+					<div class="container">
+						<h1 class="page-title">
+							<?php printf( __("Syndic de faillite à %s", 'sosendettement'), single_cat_title('', false) ); ?>
+						</h1>
+						<?php wpbp_loop_before(); ?>
+						<?php get_template_part('loop', 'repertoire'); ?>
+						<?php wpbp_loop_after(); ?>
+					</div>
+					<?php wpbp_main_inside_after(); ?>
+				</section>
+				<?php wpbp_main_after(); ?>
+			</div>
+			<div class="<?php wpbp_option('sidebar_class'); ?>">
+				<?php wpbp_sidebar_before(); ?>
+				<aside id="sidebar" role="complementary">
+					<?php wpbp_sidebar_inside_before(); ?>
+					<div class="container">
+						<?php get_sidebar(); ?>
+					</div>
+					<?php wpbp_sidebar_inside_after(); ?>
+				</aside>
+				<?php wpbp_sidebar_after(); ?>
+			</div>
+		</div>
+	</section>
+	<?php wpbp_content_after(); ?>
+<?php get_footer(); ?>
---
+++ loop-syndic.php
@@ -0,0 +1,47 @@
+<?php /* Start loop */ ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<?php wpbp_post_before(); ?>
+	<article id="syndic-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php wpbp_post_inside_before(); ?>
+		<?php if ( has_post_thumbnail() ) : ?>
+			<section class="syndic-image">
+				<?php the_post_thumbnail('medium'); ?>
+			</section>
+		<?php endif; ?>
+		<section class="syndic-content">
+			<div class="vcard">
+				<?php
+					$vcard = array(
+						'org'				=> get_the_title(),
+						'street_address'	=> get_post_meta( get_the_ID(), 'street_address', true ),
+						'street_address_2'	=> get_post_meta( get_the_ID(), 'street_address_2', true ),
+						'locality'			=> get_post_meta( get_the_ID(), 'locality', true ),
+						'region'			=> get_post_meta( get_the_ID(), 'region', true ),
+						'postal_code'		=> get_post_meta( get_the_ID(), 'postal_code', true ),
+						'country_name'		=> get_post_meta( get_the_ID(), 'country_name', true )
+					);
+				?>
+				<h3 class="org"><?php the_title(); ?></h3>
+				<p class="adr">
+					<?php echo '<span class="street-address">' . $vcard['street_address'] . '</span>' . ( $vcard['street_address_2'] ? ', <span class="street_address_2">' . $vcard['street_address_2'] . '</span>' : '' ) . '<br />'; ?>
+					<span class="locality"><?php echo $vcard['locality']; ?></span><br />
+					<span class="region"><?php echo $vcard['region']; ?></span> <span class="postal-code"><?php echo $vcard['postal_code']; ?></span><br />
+					<span class="country-name"><?php echo $vcard['country_name']; ?></span>
+				</p>
+				<p>
+					<?php _e("Téléphone :", 'sosendettement'); ?> <span class="tel"><?php echo $vcard['tel']; ?></span>
+				</p>
+				<p>
+					<?php _e("Courriel :", 'sosendettement'); ?> <a class="email" href="mailto:<?php echo $vcard['email']; ?>"><?php echo $vcard['email']; ?></a>
+				</p>
+				<p>
+					<?php _e("Site Internet :", 'sosendettement'); ?> <a class="url" href=<?php echo $vcard['url']; ?>" target="_blank"><?php echo $vcard['url']; ?></a>
+				</p>
+			</div>
+			<?php the_content(); ?>
+		</section>
+		<div class="clear"></div>
+		<?php wpbp_post_inside_after(); ?>
+	</article>
+	<?php wpbp_post_after(); ?>
+<?php endwhile; // End the loop ?>
