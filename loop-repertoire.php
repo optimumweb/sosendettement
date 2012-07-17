404dd0148fa178ac4decd75e792b88892c37fda1
Jonathan Roy <jroy@optimumweb.ca>
2012-07-17 14:13:21 -0400

    Style

--- loop-repertoire.php
+++ loop-repertoire.php
@@ -1,29 +1,47 @@
 <?php /* Start loop */ ?>
 <?php while ( have_posts() ) : the_post(); ?>
 	<?php wpbp_post_before(); ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="syndic-<?php the_ID(); ?>" <?php post_class('loop'); ?>>
 		<?php wpbp_post_inside_before(); ?>
-		<header class="post-header">
-			<h1 class="post-title">
-				<?php the_title(); ?>
-			</h1>
-			<section class="post-meta">
-				<span class="post-author byline author vcard"><?php _e('Rédigé par', 'sosendettement'); ?> <?php the_author_posts_link(); ?></span>
-				<time class="post-date updated" datetime="<?php the_time('c'); ?>" pubdate><?php printf(__('le %s', 'sosendettement'), get_the_time(__('l, F jS, Y', 'wpbp'))); ?></time>
-			</section>
-		</header>
 		<?php if ( has_post_thumbnail() ) : ?>
-			<section class="post-image">
-				<?php the_post_thumbnail('medium'); ?>
+			<section class="syndic-image">
+				<?php the_post_thumbnail('thumbnail'); ?>
 			</section>
 		<?php endif; ?>
-		<section class="post-content">
-			<?php the_content(); ?>
+		<section class="syndic-content">
+			<div class="vcard">
+				<?php
+					$vcard = array(
+						'org'				=> get_the_title(),
+						'street_address'	=> get_post_meta( get_the_ID(), 'street_address', true ),
+						'street_address_2'	=> get_post_meta( get_the_ID(), 'street_address_2', true ),
+						'locality'			=> get_post_meta( get_the_ID(), 'locality', true ),
+						'region'			=> get_post_meta( get_the_ID(), 'region', true ),
+						'postal_code'		=> get_post_meta( get_the_ID(), 'postal_code', true ),
+						'country_name'		=> get_post_meta( get_the_ID(), 'country_name', true ),
+						'tel'				=> get_post_meta( get_the_ID(), 'tel', true ),
+						'email'				=> get_post_meta( get_the_ID(), 'email', true ),
+						'url'				=> get_post_meta( get_the_ID(), 'url', true )
+					);
+				?>
+				<h3 class="org"><?php the_title(); ?></h3>
+				<p class="adr">
+					<?php echo '<span class="street-address">' . $vcard['street_address'] . '</span>' . ( $vcard['street_address_2'] ? ', <span class="street_address_2">' . $vcard['street_address_2'] . '</span>' : '' ) . ','; ?>
+					<span class="locality"><?php echo $vcard['locality']; ?></span>,
+					<span class="region"><?php echo $vcard['region']; ?></span> <span class="postal-code"><?php echo $vcard['postal_code']; ?></span>,
+					<span class="country-name"><?php echo $vcard['country_name']; ?></span>
+				</p>
+				<p>
+					<?php _e("Téléphone :", 'sosendettement'); ?> <span class="tel"><?php echo $vcard['tel']; ?></span>
+				</p>
+				<p>
+					<?php _e("Courriel :", 'sosendettement'); ?> <a class="email" href="mailto:<?php echo $vcard['email']; ?>"><?php echo $vcard['email']; ?></a>
+				</p>
+				<p>
+					<?php _e("Site Internet :", 'sosendettement'); ?> <a class="url" href=<?php echo $vcard['url']; ?>" target="_blank"><?php echo $vcard['url']; ?></a>
+				</p>
+			</div>
 		</section>
-		<footer class="post-footer">
-			<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'wpbp'), 'after' => '</p></nav>' )); ?>
-			<p class="post-tags"><?php the_tags(); ?></p>
-		</footer>
 		<div class="clear"></div>
 		<?php wpbp_post_inside_after(); ?>
 	</article>
