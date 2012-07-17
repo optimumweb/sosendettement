d40c480f504fa87393b973d1b51e96acd141abd7
Jonathan Roy <jroy@optimumweb.ca>
2012-07-17 12:30:45 -0400

    Style

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,34 @@
-$(document).ready(function() {
+var $document = $(document);
+
+$document.ready(function() {
 
 	var $buttons = $('.button');
 	$buttons.each(function() {
 		var $this = $(this);
-		$this.html('<span>' + $this.html()  + '</span>');
+		$this.html( '<span>' + $this.html()  + '</span>' );
 	});
 	
 	var $postTitles = $('.post-title');
 	$postTitles.each(function() {
-		var $this = $(this),
-			$a = $this.find('a');
-		$a.html('<span>' + $a.html()  + '</span>');
+		var $this = $(this);
+		var $a = $this.find('a');
+		$a.html( '<span>' + $a.html()  + '</span>' );
 	});
 	
-	$('a.scroll-to, .scroll-to > a').click(function(e) {
+	var $pageTitles = $('.page-title');
+	$pageTitles.each(function() {
+		var $this = $(this);
+		$this.html( '<span>' + $this.html() + '</span>' );
+	});
+	
+	var $scrollTos = $('a.scroll-to, .scroll-to > a');
+	$scrollTos.click(function(e) {
 		e.preventDefault();
-		var $this = $(this),
-			href = $this.attr('href'),
-			$href = $(href),
-			duration = $this.data('duration') || 600;
-		$.scrollTo(
-			$href,
-			duration,
-			{
-				onAfter: function() {
-					window.location.hash = href;
-				}
-			}
-		);
+		var $this = $(this);
+		var href = $this.attr('href');
+		var $href = $(href);
+		var duration = $this.data('duration') || 600;
+		$.scrollTo( $href, duration, { onAfter: function() { window.location.hash = href; } } );
 	});
 	
 });
