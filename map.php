b7441824d57692c52fd97569e426b6e01981c19d
Jonathan Roy <jroy@optimumweb.ca>
2013-07-18 15:10:07 -0400

    New map

--- map.php
+++ map.php
@@ -4,6 +4,6 @@
 			<div class="title1"><?php _e("Trouvez de l'aide près de chez vous!", 'sosendettement'); ?></div>
 			<div class="title2"><?php _e("Carte de syndics de faillite et de conseillers au Québec", 'sosendettement'); ?></div>
 		</div>
-		<iframe id="gmap" width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=205952299623759320774.0004c389985105eb3ec96&amp;ie=UTF8&amp;ll=45.500794,-73.782083&amp;spn=0.491925,0.690322&amp;t=m&amp;output=embed"></iframe>
+		<iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=205952299623759320774.0004c389985105eb3ec96&amp;ie=UTF8&amp;t=m&amp;ll=47.029059,-76.436947&amp;spn=3.548455,6.000051&amp;output=embed"></iframe>
 	</div>
 </section>
