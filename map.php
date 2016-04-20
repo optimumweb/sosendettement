0e578d90f9e6b332bff9c9aff4ca863b97d5733e
Jonathan Roy <jroy@optimumweb.ca>
2016-04-19 22:44:20 -0400

    upgrade

--- map.php
+++ map.php
@@ -1,8 +1,8 @@
 <section id="map">
-	<div class="container">
+	<div class="container fluid">
 		<div id="map-title">
 			<div class="title1"><?php _e("Trouvez de l'aide près de chez vous!", 'sosendettement'); ?></div>
-			<div class="title2"><?php _e("Carte de syndics de faillite et de conseillers au Québec", 'sosendettement'); ?></div>
+			<div class="title2"><?php _e("Carte de syndics autorisés en insolvabilité au Québec", 'sosendettement'); ?></div>
 		</div>
 		<iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=205952299623759320774.0004c389985105eb3ec96&amp;ie=UTF8&amp;t=m&amp;ll=47.029059,-76.436947&amp;spn=3.548455,6.000051&amp;output=embed"></iframe>
 	</div>
